5bdc10e9663f6e0e1590f54844ca7f9d92444ffe
Hoang Mai <hoangmai2139@gmail.com>
2024-07-08 02:41:52 +1000

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/rapidmap_test/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.rapidmap_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/sudoku.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Sudoku extends StatefulWidget {
+  const Sudoku({super.key});
+
+  @override
+  State<Sudoku> createState() => _SudokuState();
+}
+
+class _SudokuState extends State<Sudoku> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
